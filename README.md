5cd8c5e9e6ce7626b29467fd729cd94f9791ada2
whaleAlbino <100727797+whaleAlbino@users.noreply.github.com>
2022-05-22 12:49:46 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pytest_first_test
+ 19.2.3
---
+++ tests/first_tests.py
@@ -0,0 +1,20 @@
+import pytest
+from app.calculator import Calculator
+
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_division_calculate_correctly(self):
+        assert self.calc.division(self, 4, 2) == 2
+
+    def test_subtraction_calculate_correctly(self):
+        assert self.calc.subtraction(self, 5, 2) == 3
+
+    def test_adding_calculate_correctly(self):
+        assert self.calc.adding(self, 3, 2) == 5
+
